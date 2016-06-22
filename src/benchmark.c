f455b391e78b25f6f5aff69efe2c6580790b2488
Alberto Cavadia <alberto.cavadia@hotmail.com>
2016-06-22 18:33:50 -0400

    Initial structure

---
+++ src/benchmark.c
@@ -0,0 +1,28 @@
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <float.h>
+#include <getopt.h>
+#include <math.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#include <netinet/tcp.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/time.h>
+
+#include <openflow/openflow.h>
+
+#define PROG_TITLE "USAGE: ofc [option]  # by Alberto Cavadia and Daniel Tovar 2016"
+
+int main(int argc, char * argv[])
+{
+
+
+}
